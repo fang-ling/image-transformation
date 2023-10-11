13d9cddadcf930cad93ea22da2dd492f3020b340
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2023-10-11 11:04:06 +0800

    Add lowpass filter

--- Tests/ImageTransformationTests/LowpassFilterTests.swift
+++ Tests/ImageTransformationTests/LowpassFilterTests.swift
@@ -1,5 +1,5 @@
 //
-//  HistogramEqualizationTests.swift
+//  LowpassFilterTests.swift
 //  
 //
 //  Created by Fang Ling on 2023/10/11.
@@ -10,12 +10,15 @@ import ImageCodec
 @testable import ImageTransformation
 import XCTest
 
-final class HistogramEqualizationTests : XCTestCase {
+final class LowpassFilterTests : XCTestCase {
   func test() {
-    let files = ["/tmp/1.heic", "/tmp/bridge.heic"]
+    let rounds = 1
+    let files = ["/tmp/1.heic", "/tmp/moon.heic"]
     for file in files {
       var buf = image_decode(file_path: file)!
-      buf = histogram_equalization(src_buf: buf)
+      for _ in 0 ..< rounds {
+        buf = lowpass_filter(src_buf: buf, k_size: 3)
+      }
       image_encode(file_path: file, pixel_buffer: buf, quality: 1.0)
     }
   }
